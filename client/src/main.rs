4045a6a526c9c3b773658fc86aaa18a552674aec
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-07-27 15:21:29 +0900

    error handling in client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,16 @@
-use std::io::{Write, stdin, stdout};
+use std::{
+    error::Error,
+    io::{Write, stdin, stdout},
+};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpStream,
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     // TCP サーバーへ非同期で接続を試みる
-    let mut stream = TcpStream::connect("127.0.0.1:8000").await.unwrap();
+    let mut stream = TcpStream::connect("127.0.0.1:8000").await?;
     println!("✅️ Connect to KVS server.");
 
     let (reader, mut writer) = stream.split();
@@ -22,13 +25,13 @@ async fn main() {
         print!("> ");
         // ☝️ の print!() は改行を含まないため直ぐに表示されない可能性があるので
         // flush() でバッファを強制的に空にして、"> " をすぐに画面出力させる
-        stdout().flush().unwrap();
+        stdout().flush()?;
 
         // ユーザー入力の受け取り
         // read_line() プログラムの実行を一時停止して、ユーザーが入力して Enter を押すのを待つ
         // Enter が押されたら、改行を含んだ文字列が user_input に格納される
         let mut user_input = String::new();
-        stdin().read_line(&mut user_input).unwrap();
+        stdin().read_line(&mut user_input)?;
 
         // 入力が改行のみの場合は、何もせずループの先頭に戻る
         if user_input.trim().is_empty() {
@@ -37,17 +40,17 @@ async fn main() {
 
         if user_input.trim() == "exit" {
             println!("👋 Exiting.");
-            // サーバーへ exit コマンドを送信して終了
-            writer.write_all(user_input.as_bytes()).await.unwrap();
             break;
         }
 
         // ユーザー入力をサーバーに送信
-        writer.write_all(user_input.as_bytes()).await.unwrap();
+        writer.write_all(user_input.as_bytes()).await?;
 
         // サーバーからの応答を読み取って表示
         server_response.clear();
-        reader.read_line(&mut server_response).await.unwrap();
+        reader.read_line(&mut server_response).await?;
         println!("{}", server_response);
     }
+
+    Ok(())
 }
